5013b56659d5429df91565aa4a4f9de3c1ecb67c
Huy Luong <huylvgcs190132@fpt.edu.vn>
2021-11-27 10:12:58 +0700

    ESlint, Express and Update Server code

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import { data } from '../data';
 import { Product } from '../components/Products';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get('/api/products');
+        setLoading(false);
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div className='row center'>
-      {data.products.map((product) => (
-        <Product product={product} />
-      ))}
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant='danger'>{error}</MessageBox>
+      ) : (
+        <div className='row center'>
+          {products.map((product) => (
+            <Product product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
